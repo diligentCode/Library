dd28404bef8a5bb2d27ce48ff2cb2d1358a0bda0
diligentCode <anuditjangid@gmail.com>
2025-01-31 13:27:12 +0530

    Add javascript logic for the elements
    We havent commited freq as this was tiresum from next time
    We will try to commit everything

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // Selecting the constants
 const bookContent = document.querySelector(".book-content");
+const newBookbtn = document.querySelector(".create");
+const dialogForm = document.querySelector(".my-dialog");
+const cancelbtn = document.querySelector(".cancel");
+const submitbtn = document.querySelector(".submit");
 
-const myLibrary = [];
+// Array to store books
+let myLibrary = [];
 
 // Creating some default books
 // Default book1
@@ -19,10 +24,52 @@ b2.bookPages = 213;
 b2.bookDiscription = "Book based on sprituality";
 b2.bookimg = "Assets/book2.jpg";
 
+// Push these books in librara
 myLibrary.push(b1);
 myLibrary.push(b2);
 // Display these books
 displayBook();
+
+// Open dailog page when button is clicked
+newBookbtn.addEventListener("click", () => {
+  dialogForm.showModal();
+});
+
+// submit button
+submitbtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  // Now select the form elements
+  let imageFile = document.getElementById("image-input");
+  let name = document.getElementById("book-name");
+  let author = document.getElementById("book-author");
+  let pages = document.getElementById("book-pages");
+  let discription = document.getElementById("book-discription");
+
+  // Create a book
+  const newBook = new Book();
+  newBook.bookimg = URL.createObjectURL(imageFile.files[0]);
+  newBook.bookName = name.value;
+  newBook.bookAuthor = author.value;
+  newBook.bookPages = pages.value;
+  newBook.bookDiscription = discription.value;
+
+  // for (let key in newBook) {
+  //   alert(newBook.key);
+  // }
+
+  myLibrary.push(newBook);
+  displayBook();
+
+  dialogForm.close();
+
+  // update the borders
+  updateBorderOutside();
+});
+
+// Close the dialog form
+cancelbtn.addEventListener("click", () => {
+  dialogForm.close();
+});
 
 // Creating a book constructor
 function Book() {
@@ -33,11 +80,11 @@ function Book() {
   this.bookimg;
 }
 
-// Function to add books to libray
-function addBookToLibrary(myLibrary) {}
-
 // Funciton to display book
 function displayBook() {
+  // Clearing inner html so that books are not repeated
+  bookContent.innerHTML = "";
+
   myLibrary.forEach((b) => {
     //Create element and append in the html for books
     // book-card
@@ -62,14 +109,28 @@ function displayBook() {
               <p>${b.bookPages}</p>
             </div>
             <div class="discription">
-              <b>Discription</b>
+              <b>Discription:-</b>
               <i>${b.bookDiscription}</i>
             </div>
             <div class="button">
               <label for="read"><b>Read</b></label>
               <input type="checkbox" id="read" />
-              <button class="delte">Delete</button>
+              <button class="delete">Delete</button>
             </div>`;
+
+    // Delete a book
+    // Add event listener for delete button
+    const deletebtn = bookCard.querySelector(".delete");
+
+    deletebtn.addEventListener("click", () => {
+      let delBookName = bookCard.querySelector(".name p").textContent;
+
+      // Remove the book from the library array
+      myLibrary = myLibrary.filter((book) => book.bookName !== delBookName);
+
+      // Re-display books
+      displayBook();
+    });
   });
 }
 
@@ -79,3 +140,24 @@ const inputImage = document.getElementById("image-input");
 inputImage.addEventListener("change", () => {
   bookImage.setAttribute("src", URL.createObjectURL(inputImage.files[0]));
 });
+
+// Changing the border color based on read or not read
+// Select each book card and run querryselector on that as we want to change that perticular image
+
+function updateBorderOutside() {
+  document.querySelectorAll(".book-card").forEach((bookCard) => {
+    const checkbox = bookCard.querySelector("input[type='checkbox']");
+    const bookImage = bookCard.querySelector("img");
+
+    function updateBorder() {
+      bookImage.style.border = checkbox.checked
+        ? "4px solid #00fbff"
+        : "4px solid #ef32d9";
+    }
+
+    updateBorder();
+
+    checkbox.addEventListener("change", updateBorder);
+  });
+}
+updateBorderOutside();
